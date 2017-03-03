a6c09da88cd45e3cd741f653e0ff3043f3f51255
sk00t3r <johnsone@roadrunner.com>
2017-03-03 08:34:01 -0500

    Update check_smb_file.pl

--- check_smb_file.pl
+++ check_smb_file.pl
@@ -539,7 +539,7 @@ if ($o_filename_match || $o_mode_directory) {
     }
     foreach my $filename ($smb->readdir($fd)) {
         my $full_filename = "$full_file_path/$filename";
-        if($filename eq "." || $filename eq ".."){ next;}
+        if($filename eq "." || $filename eq ".."){next;}
         if (($o_filename_match and !$o_match_case) and $filename =~ m/$o_filename_match/i) {
             $directory_files{"$o_filepath/$filename"} = \@{ getFileStat($full_filename, $smb) };
         }
@@ -581,7 +581,7 @@ if ($o_filename_match || $o_mode_directory) {
     }
     $smb->close($fd);
     if (scalar keys %directory_files == 0) {
-        showOutputAndExit("No files found",'CRITICAL');
+        showOutputAndExit("No files found",'OK');
     }
     if (scalar @critical_errors || scalar @warning_errors) {
         showOutputAndExit(
